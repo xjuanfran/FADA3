c381394cf064cfc76c649f4d41a2a748435fccea
elkoreano <criistiian-95@hotmail.com>
2023-06-04 14:47:23 -0500

    operacion borrado

--- index.js
+++ index.js
@@ -21,6 +21,17 @@ const addNode = () => {
   }
 };
 
+// Delete operation
+const deleteNode = () => {
+  let value = parseInt(document.getElementById("value-node").value);
+  if (!isNaN(value)) {
+    root = deleteFromTree(root, value);
+    drawTree();
+  } else {
+    alert("Ingrese un valor numerico");
+  }
+};
+
 const insertNode = (root, value) => {
   if (root === null) {
     // Si la raíz es nula, creamos un nuevo nodo con el valor dado y lo asignamos como la raíz
@@ -37,6 +48,27 @@ const insertNode = (root, value) => {
       // Si el valor es mayor o igual que el valor de la raíz, lo insertamos en el subárbol derecho
       root.right = insertNode(root.right, value);
     }
+  }
+  return root;
+};
+
+// Delete operation
+const deleteFromTree = (root, value) => {
+  if (root === null) {
+    return root;
+  }
+  if (value < root.value) {
+    root.left = deleteFromTree(root.left, value);
+  } else if (value > root.value) {
+    root.right = deleteFromTree(root.right, value);
+  } else {
+    if (root.left === null) {
+      return root.right;
+    } else if (root.right === null) {
+      return root.left;
+    }
+    root.value = findMinValue(root.right);
+    root.right = deleteFromTree(root.right, root.value);
   }
   return root;
 };
